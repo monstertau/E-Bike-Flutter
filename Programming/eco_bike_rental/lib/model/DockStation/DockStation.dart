622aefdd8603d350bf1950397c9f8a2a367480fc
miasnguyenx <56225902+miasnguyenx@users.noreply.github.com>
2020-11-29 19:29:53 +0700

    Test driven dock controller, dock model (#32)
    * Done UnitTest - DockController + DockModel
    
    * Delete .idea file
    
    * Done DockController + DockModel
    
    Co-authored-by: nguyenxnghiax <nguyentrungnghia.cnh@gmail.com>
    Co-authored-by: nxhoanghust <nxhoang99@gmail.com>

--- Programming/eco_bike_rental/lib/model/DockStation/DockStation.dart
+++ Programming/eco_bike_rental/lib/model/DockStation/DockStation.dart
@@ -9,6 +9,9 @@ class DockStation {
   List<Bike> _lstBike;
 
   int get dockID => id;
+  DockStation.origin(){
+  }
+  DockStation(this.id, this._dockName, this._dockArea, this._dockAddress, this._dockSize, this._lstBike);
 
   String get dockName => _dockName;
 
@@ -39,15 +42,20 @@ class DockStation {
   set lstBike(List<Bike> value) {
     _lstBike = value;
   }
-
+  DockStation getDockById(int id){
+    // TODO: fix this
+    List<Bike> aListBike = new List<Bike>();
+    return new DockStation(123,'abc','12x12','abc123',23,aListBike);
+  }
   List getAllDock() {
-    // TODO: implement this
-    //db = new dbconnect();
-    //list<String> result;
-    //result = dbconnect->getAllRow(DockStation);
-    //return result;
-
-    List<String> result = new List<String>();
-    return result;
+  //   // TODO: implement this
+  //   //db = new dbconnect();
+  //   //list<String> result;
+  //   //result = dbconnect->getAllRow(DockStation);
+  //   //return result;
+  //
+  //   List<String> result = new List<String>();
+  //   return result;
+    return List<DockStation>();
   }
 }
