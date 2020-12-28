fc3032134b5e981584fadfc1e055b10e89d1893c
Trung Dung Vu <trungdungvu172@gmail.com>
2020-12-28 12:56:56 +0700

    add some test

--- Programming/eco_bike_rental/test/model/DB/db_boundary_test.dart
+++ Programming/eco_bike_rental/test/model/DB/db_boundary_test.dart
@@ -1,5 +1,9 @@
+import 'dart:convert';
+
 import 'package:eco_bike_rental/model/DB/db_boundary.dart';
 import 'package:flutter_test/flutter_test.dart';
+
+import 'package:crypto/crypto.dart';
 
 void main() {
   group('Test Generate Header', () {
@@ -10,6 +14,12 @@ void main() {
         "/dock/get-detail",
         "GROUP10 841a6ad1b24833c266b8683ab63fc368348fa913:YzI3M2RhNWE2MGY3NWFhNTc0NmRkZmNmNjI1YTIxY2RkYjg4ZDI5Mw=="
       },
+      {
+        "POST",
+        '{"dockId":1,"bikeId":99}',
+        "/bike/return-to-dock",
+        "GROUP10 841a6ad1b24833c266b8683ab63fc368348fa913:ZGZlNDUwZDgzNzg3YjAwYTZhNmMxNzE1MTY0N2Y0NzY3NjhhN2IwMQ=="
+      }
     ];
     testCase.forEach((element) {
       test("Test $element", () {
@@ -19,5 +29,10 @@ void main() {
         expect(res, expected);
       });
     });
+  });
+  test("test MD5 Hash", () {
+    Map body = {"dockId": 1, "bikeId": 99};
+    var test = md5.convert(utf8.encode(jsonEncode(body))).toString();
+    print(test);
   });
 }
